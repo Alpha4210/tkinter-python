b614a1b70fafda0effb45ab1677af65980a5e40b
Alpha4210 <alpha.gamer.2427@gmail.com>
2024-08-12 19:37:12 +0530

    Dance form Quick Quiz completed

--- Quick_Quiz_dance_class.py
+++ Quick_Quiz_dance_class.py
@@ -1,6 +1,15 @@
 from tkinter import *
+import tkinter.messagebox as tmb
+import csv
 root = Tk()
-root.geometry("1000x500")
+root.geometry("330x500")
+def saveval():
+    tmb.showinfo("Thank You","Thanks for submitting the form,  we will contact you soon!")
+    with open("save_dance_record.csv", "a+") as f:
+        x = [namevalue.get(), emailvalue.get(), phonevalue.get(), agevalue.get(), dancevalue.get()]
+        fwr = csv.writer(f)
+        fwr.writerow(x)
+Label(root, text="Dance Form", font="verdana 19 bold").grid(row=0, column=1)
 
 #Text for our form
 name = Label(root, text="Name")
@@ -10,14 +19,35 @@ age = Label(root, text="Age")
 dance = Label(root, text="Type of dance")
 
 #Packing the text
-name.grid(row=1, column=0)
-email.grid(row=2, column=0)
-phone.grid(row=3, column=0)
-age.grid(row=4, column=0)
-dance.grid(row=5, column=0)
+name.grid(row=1, column=0, pady=13, padx=5)
+email.grid(row=2, column=0, pady=13, padx=5)
+phone.grid(row=3, column=0, pady=13, padx=5)
+age.grid(row=4, column=0, pady=13, padx=5)
+dance.grid(row=5, column=0, pady=13, padx=5)
 
+#Variables for storing entries
+namevalue = StringVar()
+emailvalue = StringVar()
+phonevalue = StringVar()
+agevalue = StringVar()
+dancevalue = StringVar()
+
+#Input box for getting values
+nameentry = Entry(root, textvariable=namevalue)
+emailentry = Entry(root, textvariable=emailvalue)
+phoneentry = Entry(root, textvariable=phonevalue)
+ageentry = Entry(root, textvariable=agevalue)
+danceentry = Entry(root, textvariable=dancevalue)
+
+#Packing the input boxes
+nameentry.grid(row=1, column=1, pady=13, padx=5)
+emailentry.grid(row=2, column=1, pady=13, padx=5)
+phoneentry.grid(row=3, column=1, pady=13, padx=5)
+ageentry.grid(row=4, column=1, pady=13, padx=5)
+danceentry.grid(row=5, column=1, pady=13, padx=5)
 #Creating input boxes
 
+Button(text="Submit", command=saveval, padx=20, pady=12, bg="lightgreen").grid(row=7, column=1)
 
 
 
