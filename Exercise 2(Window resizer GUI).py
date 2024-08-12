651fedace3596e1987184ae381ad34a99c70dcb2
Alpha4210 <alpha.gamer.2427@gmail.com>
2024-08-12 13:38:33 +0530

    Video 20 completed

--- Exercise 2(Window resizer GUI).py
+++ Exercise 2(Window resizer GUI).py
@@ -1,2 +1,3 @@
 # Create a GUI window that takes the input of width and height
 # Create a Button "Apply" and after clicking on this button it will change the size of the GUI window accordingly
+
---
+++ day7 12-8-24.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import tkinter.messagebox as tmsg
+root = Tk()
+root.geometry("455x233")
+root.title("ABC")
+def order():
+    tmsg.showinfo("Order recieved", f"We have recieved your order for {var.get()}. thanks for ordering")
+var = IntVar()
+# var.set(1) #used to set a value to a variable.
+Label(root, text="What would you like to have?", justify=LEFT, padx=14, font="lucida 19 bold").pack()
+
+radio = Radiobutton(root, text="Dosa", padx=14, variable=var, value=1).pack(anchor=W) #Used to create a radio button
+radio = Radiobutton(root, text="Idli", padx=14, variable=var, value=2).pack(anchor=W)
+radio = Radiobutton(root, text="Paratha", padx=14, variable=var, value=3).pack(anchor=W)
+radio = Radiobutton(root, text="Samosa", padx=14, variable=var, value=4).pack(anchor=W)
+
+#Small task 1 - Try to create these radiobutton using for loop
+
+Button(text="order now", command=order).pack()
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+#  Videos - 21, 
